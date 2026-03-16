b1ae4151fc00ff535257de757f6f36c335b40ffd
Peibing Gu <peibing@iMac.modem>
2026-03-16 16:57:09 +1100

    Added prac1 files

--- SIT725-2026-T1-Prac1/js/scripts.js
+++ SIT725-2026-T1-Prac1/js/scripts.js
@@ -1,17 +1,12 @@
-function changeText(){
+'use strict';
 
-  var textsArray = ["Text 1","Text 2", "Text 3", "Text 4", "Text 5"]
+const TEXTS = ['Text 1', 'Text 2', 'Text 3', 'Text 4', 'Text 5'];
 
-  var number = getRandomNumberBetween(0,textsArray.length - 1)
-
-  console.log("Index: ", number)
-
-  document.getElementById("heading").innerHTML = textsArray[number];
-
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function getRandomNumberBetween(min,max){
-
-  return Math.floor(Math.random()*(max-min+1)+min);
-
+function changeText() {
+  const index = getRandomInt(0, TEXTS.length - 1);
+  document.getElementById('heading').textContent = TEXTS[index];
 }
